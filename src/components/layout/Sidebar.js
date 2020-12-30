14cf67a0ee266900cccf241134242789f3a342d9
yarocruz <yarocruz@gmail.com>
2020-12-29 22:21:37 -0500

    started adding firebase

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -1,12 +1,29 @@
+import {FaInbox, FaRegCalendarAlt, FaRegCalendar, FaChevronDown} from "react-icons/all";
 
 function Sidebar () {
     return (
         <div className="sidebar" data-testid="sidebar">
-            <ul className="sidebar_generic">
-                <li>Inbox</li>
-                <li>Today</li>
-                <li>Next 7 Days</li>
+            <ul className="sidebar__generic">
+                <li>
+                    <span><FaInbox /></span>
+                    <span>Inbox</span>
+                </li>
+                <li>
+                    <span><FaRegCalendar /></span>
+                    <span>Today</span>
+                </li>
+                <li>
+                    <span><FaRegCalendarAlt /></span>
+                    <span>Next 7 Days</span>
+                </li>
             </ul>
+            <div className="sidebar__middle">
+                <span><FaChevronDown /></span>
+                <h2>Projects</h2>
+            </div>
+            <div className="sidebar__projects">Projects will be here!</div>
+
+            Add Project Component Here!
         </div>
     )
 }
